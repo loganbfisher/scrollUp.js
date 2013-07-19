6d237a57c2207a29029374bb00f2b0fabc302cc0
Logan Fisher <loganbfisher@gmail.com>
2013-07-18 23:13:22 -0500

    initial upload

---
+++ scrollup.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+$(document).ready(function(){
+var ScrollUpInit = $('body').hasClass('scrollUp');
+
+ 	// Initiate  scrollUp.
+	if(ScrollUpInit) {
+		var scrollTab = $('<div class="scroll-tab">scroll tab</div>');
+		$('body').append(scrollTab);
+		
+		// Show scrollTab.
+		$(window).scroll(function(){
+			var scrollOffset = $('body').scrollTop();
+			
+			if(scrollOffset < 100){
+				scrollTab.slideUp();
+			}
+			else {
+				scrollTab.slideDown();
+			}
+		});
+	
+		scrollTab.on("click", function() {
+			$("body").animate({ scrollTop: 0 });
+		});
+  }
+	
+});
