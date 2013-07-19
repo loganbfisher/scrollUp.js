55c922e864969fbfa3430940fa4b2279c286e3f7
Logan Fisher <loganbfisher@gmail.com>
2013-07-19 08:40:48 -0500

    Update scrollup.js

--- scrollup.js
+++ scrollup.js
@@ -2,26 +2,26 @@
 $(document).ready(function(){
 var ScrollUpInit = $('body').hasClass('scrollUp');
 
- 	// Initiate  scrollUp.
-	if(ScrollUpInit) {
-		var scrollTab = $('<div class="scroll-tab">scroll tab</div>');
-		$('body').append(scrollTab);
-		
-		// Show scrollTab.
-		$(window).scroll(function(){
-			var scrollOffset = $('body').scrollTop();
-			
-			if(scrollOffset < 100){
-				scrollTab.slideUp();
-			}
-			else {
-				scrollTab.slideDown();
-			}
-		});
+ // Initiate  scrollUp.
+if(ScrollUpInit) {
+	var scrollTab = $('<div class="scroll-tab">scroll tab</div>');
+	$('body').append(scrollTab);
 	
-		scrollTab.on("click", function() {
-			$("body").animate({ scrollTop: 0 });
-		});
-  }
+	// Show scrollTab.
+	$(window).scroll(function(){
+		var scrollOffset = $('body').scrollTop();
+		
+		if(scrollOffset < 100){
+			scrollTab.slideUp();
+		}
+		else {
+			scrollTab.slideDown();
+		}
+	});
+	
+	scrollTab.on("click", function() {
+		$("body").animate({ scrollTop: 0 });
+	});
+}
 	
 });
